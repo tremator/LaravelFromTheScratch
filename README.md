c83f4d3da74f20e475df5bb4b75f5408e2d3a033
tremator <dmsolisa@gmial.com>
2021-10-13 16:22:28 -0600

    Escribir el Markup para un Comentario

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
     - [Search](./Documentation/Search.md)
     - [Pagination](./Documentation/Pagination.md)
     - [FormsAndAuth](./Documentation/FormsAndAuth.md)
+    - [Comments](./Documentation/Comments.md)
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,3 +1,9 @@
 <x-layout>
     <x-post_card :post="$post"></x-post_card_blade>
+
+    <section class="col-span-8 col-start-5 mt-10 space-y-6"> 
+        <x-post_comment></x-post_comment>
+     
+
+    </section>    
 </x-layout>
